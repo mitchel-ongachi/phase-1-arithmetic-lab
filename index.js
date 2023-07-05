49242b0940129a029b50ef14db7795e97462af3e
mitchel buyengo <mitchelongachi63@gmail.com>
2023-07-05 14:34:38 +0300

    work almost done

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1=2
+const num2=31
+let multiply= num1 * num2
 
-// Write your code here
+let num6=Math.floor (Math.random()*10)+1
+  
+  const num3=24
+  const num4=6
+  let mod=num3 / num4
+
+  const max=Math.max(5,10,20)
