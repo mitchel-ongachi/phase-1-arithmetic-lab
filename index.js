9cdfc0b35574dd024664a2153b96424f4a6014df
mitchel buyengo <mitchelongachi63@gmail.com>
2023-07-07 14:03:32 +0300

    work complete

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const num1=2
 const num2=31
 let multiply= num1 * num2
 
-let num6=Math.floor (Math.random()*10)+1
+let random = Math.floor(Math.random() * 10) + 1;
   
   const num3=24
   const num4=6
